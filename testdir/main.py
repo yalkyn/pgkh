cca631e03146967143f152a24b89d35f4613891a
yalkyn-termux <victrola.766@gmail.com>
2022-06-15 07:10:40 +0300

    Есть прочтение файлов, создание и просмотр директории. Думаю над записью в файл.

--- testdir/main.py
+++ testdir/main.py
@@ -32,7 +32,7 @@ def viewfiles():
 
 
 def makefile():
-    filepr = input('ВВЕДИТЕ НАЗВАНИЕ ФАЙЛА (БЕЗ РАСШИРЕНИЯ)')
+    filepr = input('ВВЕДИТЕ НАЗВАНИЕ ФАЙЛА (БЕЗ РАСШИРЕНИЯ) ')
     filepr = filepr.upper()
     create = True
     filepr += '.ТЕКСТ'
@@ -49,12 +49,38 @@ def makefile():
             pass
     if create is False:
         print('')
-        print('ДАННЫЙ ФАЙЛ УЖЕ СУЩЕСТВУЕТ. ПОПРОБУЙТЕ ДРУГОЕ НАЗВАНИЕ.')
+        print('УЖЕ СУЩЕСТВУЕТ. ПОПРОБУЙТЕ ДРУГОЕ НАЗВАНИЕ.')
         print('')
     else:
         basefile = open("kh.txt", "a")
         basefile.write('\n' + filepr + '\n')
         basefile.write('ПУСТОЙФАЙЛ')
+
+
+def readfile():
+    basefile = open("kh.txt", "r")
+    content = basefile.read()
+    contlist = content.split('\n')
+    ins = input('ВВЕДИТЕ НАЗВАНИЕ ФАЙЛА БЕЗ РАСШИРЕНИЯ ')
+    ins += '.ТЕКСТ'
+    check = False
+    lnnm = 0
+    for k in range(0, len(contlist)):
+        if ins == contlist[k]:
+            check = True
+            lnnm = k
+        else:
+            pass
+    if check is True:
+        if contlist[lnnm + 1]:
+            if contlist[lnnm + 1] != 'ПУСТОЙФАЙЛ':
+                print(contlist[lnnm + 1])
+            else:
+                print('')
+        else:
+            print('ПРОГРАММНАЯ ОШИБКА. КОД 54-107')
+    else:
+        print('НЕ СУЩЕСТВУЕТ.')
 
 
 def handle_input():
@@ -64,6 +90,12 @@ def handle_input():
         handle_input()
     elif inp == "СОЗД":
         makefile()
+        handle_input()
+    elif inp == "ПРОЧТ":
+        readfile()
+        handle_input()
+    elif inp == "ДОБАВ":
+        write_to_file()
         handle_input()
     else:
         print('КОМАНДА НЕ РАСПОЗНАНА')
